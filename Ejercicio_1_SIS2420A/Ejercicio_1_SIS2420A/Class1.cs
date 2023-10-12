5b0c7e239945360d611093640b46d7cb5ba3fce7
LuzakyGG <147016111+LuzakyGG@users.noreply.github.com>
2023-10-12 02:24:37 -0300

    Add files via upload

---
+++ Ejercicio_1_SIS2420A/Ejercicio_1_SIS2420A/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1_SIS2420A
+{
+    public static class NumeroEnLetras
+    {
+        private static string[] unidades = { "cero", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve" };
+        private static string[] decenas = { "", "", "veinte", "treinta", "cuarenta", "cincuenta", "sesenta", "setenta", "ochenta", "noventa" };
+        private static string[] especiales = { "diez", "once", "doce", "trece", "catorce", "quince", "dieciséis", "diecisiete", "dieciocho", "diecinueve" };
+        private static string[] centenas = { "", "cien", "doscientos", "trescientos", "cuatrocientos", "quinientos", "seiscientos", "setecientos", "ochocientos", "novecientos" };
+
+        public static string ConvertirNumeroEnLetras(int numero)
+        {
+            if (numero >= 0 && numero < 10)
+            {
+                return unidades[numero];
+            }
+            else if (numero >= 10 && numero < 20)
+            {
+                return especiales[numero - 10];
+            }
+            else if (numero >= 20 && numero <= 99)
+            {
+                int decena = numero / 10;
+                int unidad = numero % 10;
+                if (unidad == 0)
+                {
+                    return decenas[decena];
+                }
+                else
+                {
+                    return decenas[decena] + " y " + unidades[unidad];
+                }
+            }
+            else if (numero >= 100 && numero <= 999)
+            {
+                int centena = numero / 100;
+                int decena = numero % 100;
+                if (decena == 0)
+                {
+                    return centenas[centena];
+                }
+                else
+                {
+                    return centenas[centena] + " " + ConvertirNumeroEnLetras(decena);
+                }
+            }
+            else if (numero >= 1000 && numero <= 9999)
+            {
+                int unidadDeMil = numero / 1000;
+                int resto = numero % 1000;
+                if (resto == 0)
+                {
+                    return unidades[unidadDeMil] + " mil";
+                }
+                else
+                {
+                    return unidades[unidadDeMil] + " mil " + ConvertirNumeroEnLetras(resto);
+                }
+            }
+            else
+            {
+                return "Número fuera de rango";
+            }
+        }
+    }
+}
---
+++ Ejercicio_1_SIS2420A/Ejercicio_1_SIS2420A/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1_SIS2420A
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Numero: ");
+
+            int numero = int.Parse(Console.ReadLine());
+            string numeroEnLetras = NumeroEnLetras.ConvertirNumeroEnLetras(numero);
+            Console.WriteLine(numeroEnLetras);
+            Console.ReadKey();
+            
+            
+        }
+    }
+}
+
+
