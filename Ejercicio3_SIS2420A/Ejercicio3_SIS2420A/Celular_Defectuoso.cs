2c7bbdef4982f5de8126643de261017a1fb411d9
LuzakyGG <147016111+LuzakyGG@users.noreply.github.com>
2023-10-12 02:26:11 -0300

    Add files via upload

---
+++ Ejercicio3_SIS2420A/Ejercicio3_SIS2420A/Celular_Defectuoso.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3_SIS2420A
+{
+    public class Celular_Defectuoso : Celular
+    {
+        public string Motivo { get; set; }
+
+        public Celular_Defectuoso(string marca, string modelo, string so, int ram, int almacenamiento, DateTime fechaIngreso, string motivo)
+            : base(marca, modelo, so, ram, almacenamiento, fechaIngreso)
+        {
+            Motivo = motivo;
+        }
+
+        public override void MostrarCelular()
+        {
+            Console.WriteLine("Celular Defectuoso:");
+            Console.WriteLine($"Modelo: {Modelo}");
+            Console.WriteLine($"Marca: {Marca}");
+            Console.WriteLine($"Sistema Operativo: {SO}");
+            Console.WriteLine($"RAM: {Ram} GB");
+            Console.WriteLine($"Almacenamiento: {Almacenamiento} GB");
+            Console.WriteLine($"Fecha de Ingreso: {FechaIngreso.ToShortDateString()}");
+            Console.WriteLine($"Motivo del Defecto: {Motivo}");
+        }
+    }
+
+    
+    }
+
---
+++ Ejercicio3_SIS2420A/Ejercicio3_SIS2420A/Program.cs
@@ -0,0 +1,100 @@
+﻿using Ejercicio3_SIS2420A;
+using System;
+
+public abstract class Celular
+{
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+    public string SO { get; set; }
+    public int Ram { get; set; }
+    public int Almacenamiento { get; set; }
+    public DateTime FechaIngreso { get; set; }
+
+    
+    public Celular(string marca, string modelo, string so, int ram, int almacenamiento, DateTime fechaIngreso)
+    {
+        Marca = marca;
+        Modelo = modelo;
+        SO = so;
+        Ram = ram;
+        Almacenamiento = almacenamiento;
+        FechaIngreso = fechaIngreso;
+    }
+
+    
+    public abstract void MostrarCelular();
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Celular> celulares = new List<Celular>
+        {
+            new Celular_Nuevo("Samsung", "Galaxy S21", "Android", 8, 128, new DateTime(2023, 1, 15), 899.99m),
+            new Celular_Nuevo("Apple", "iPhone 13", "iOS", 4, 64, new DateTime(2023, 2, 5), 999.99m),
+            new Celular_Nuevo("Google", "Pixel 6", "Android", 6, 256, new DateTime(2023, 3, 10), 799.99m),
+            new Celular_Nuevo("Sony", "Xperia 5", "Android", 6, 128, new DateTime(2023, 4, 20), 699.99m),
+            new Celular_Nuevo("OnePlus", "9 Pro", "Android", 12, 256, new DateTime(2023, 5, 1), 899.99m),
+            new Celular_Nuevo("LG", "G9", "Android", 8, 128, new DateTime(2023, 6, 15), 699.99m),
+            new Celular_Defectuoso("Motorola", "Moto G7", "Android", 4, 64, new DateTime(2023, 9, 20), "Pantalla rota"),
+            new Celular_Defectuoso("Nokia", "8.3", "Android", 6, 128, new DateTime(2023, 10, 1), "Batería defectuosa"),
+        };
+
+       
+        double Prom_Celular(List<Celular_Nuevo> celularesNuevos)
+        {
+            return celularesNuevos.Average(celular => (double)celular.Precio);
+        }
+
+        
+        List<Celular_Nuevo> Cel_MarcaS(List<Celular_Nuevo> celularesNuevos)
+        {
+            return celularesNuevos.Where(celular => celular.Marca == "Samsung").ToList();
+        }
+
+        
+        List<Celular_Nuevo> Celular_RSA(List<Celular_Nuevo> celularesNuevos)
+        {
+            return celularesNuevos.Where(celular => celular.Ram == 8 && celular.SO == "Android" && celular.Almacenamiento == 128).ToList();
+        }
+
+        
+        List<Celular_Nuevo> Celular_Ingreso(List<Celular_Nuevo> celularesNuevos, int año)
+        {
+            return celularesNuevos.Where(celular => celular.FechaIngreso.Year == año).ToList();
+        }
+
+        
+        var celularesApple = from celular in celulares.OfType<Celular_Nuevo>()
+                             where celular.Marca == "Apple"
+                             select new { celular.Modelo, celular.Precio };
+
+        Console.WriteLine($"Promedio de precios de los celulares nuevos: ${Prom_Celular(celulares.OfType<Celular_Nuevo>().ToList()):0.00}");
+        Console.WriteLine("Celulares de Marca Samsung:");
+        List<Celular_Nuevo> samsungCelulares = Cel_MarcaS(celulares.OfType<Celular_Nuevo>().ToList());
+        foreach (var celular in samsungCelulares)
+        {
+            celular.MostrarCelular();
+        }
+
+        Console.WriteLine("Celulares con RAM = 8GB, SO = Android y Almacenamiento de 128GB:");
+        List<Celular_Nuevo> rsaCelulares = Celular_RSA(celulares.OfType<Celular_Nuevo>().ToList());
+        foreach (var celular in rsaCelulares)
+        {
+            celular.MostrarCelular();
+        }
+
+        Console.WriteLine("Celulares ingresados en el año 2023:");
+        List<Celular_Nuevo> ingreso2023Celulares = Celular_Ingreso(celulares.OfType<Celular_Nuevo>().ToList(), 2023);
+        foreach (var celular in ingreso2023Celulares)
+        {
+            celular.MostrarCelular();
+        }
+
+        Console.WriteLine("Modelos y precios de celulares Apple:");
+        foreach (var celular in celularesApple)
+        {
+            Console.WriteLine($"{celular.Modelo}: ${celular.Precio}");
+        }
+    }
+}
